0f04b98dadac7afacc18823ce3a2a4bbd6167e6f
BotThice <thiranut.120346@gmail.com>
2024-12-27 14:20:00 +0700

    - more FoldTree - add isBST function

--- ex11-purely-function.hs
+++ ex11-purely-function.hs
@@ -11,9 +11,19 @@ height :: Tree a -> Integer
 height Empty = 0
 height (Node l root r) = max (height l) (height r) + 1
 
-foldTree  :: (a -> b -> b -> b) -> b -> Tree a -> b
-foldTree _ acc Empty = acc
-foldTree op acc (Node l root r) = op root (foldTree op acc l) (foldTree op acc r)
+-- foldTree0 (\root l r -> root ++ l ++ r) "" tree4
+foldTree0  :: (a -> b -> b -> b) -> b -> Tree a -> b
+foldTree0 _ acc Empty = acc
+foldTree0 op acc (Node l root r) = op root (foldTree0 op acc l) (foldTree0 op acc r)
+
+-- foldTree1 (++) "" tree4
+foldTree1 :: (a -> b -> b) -> b -> Tree a -> b
+foldTree1 func acc Empty = acc
+foldTree1 func acc (Node l root r) = func root (foldTree1 func (foldTree1 func acc r) l)
+
+foldTree2 :: (b -> a -> b) -> b -> Tree a -> b
+foldTree2 func acc Empty = acc
+foldTree2 func acc (Node l root r) = func (foldTree2 func (foldTree2 func acc l) r) root
 
 preOrderNAry :: NAryTree a -> [a]
 preOrderNAry NEmpty = []
@@ -25,6 +35,33 @@ postOrderNAry NEmpty = []
 postOrderNAry (NNode root []) = [root]
 postOrderNAry (NNode root children) = concatMap postOrderNAry children ++ [root]
 
--- isBST :: Tree a -> Bool
--- isBST Empty = True
--- isBST (Node l root r) = mapBTree (<root) l && mapBTree (>root) r
+isBST :: Ord a => Tree a -> Bool
+isBST Empty = True
+isBST (Node Empty root Empty) = True
+isBST (Node Empty root r)
+    | findMin r < root = False
+    | otherwise = isBST r
+isBST (Node l root Empty)
+    | findMax l > root = False
+    | otherwise = isBST l
+isBST (Node l root r)
+    | findMax l > root = False
+    | findMin r < root = False
+    | otherwise = isBST l && isBST r
+    
+findMin :: Ord a => Tree a -> a
+findMin (Node Empty root Empty) = root
+findMin (Node l root Empty) = min root (findMin l)
+findMin (Node Empty root r) = min root (findMin r)
+findMin (Node l root r) = min root (min (findMin l) (findMin r))
+
+findMax :: Ord a => Tree a -> a
+findMax (Node Empty root Empty) = root
+findMax (Node l root Empty) = max root (findMax l)
+findMax (Node Empty root r) = max root (findMax r)
+findMax (Node l root r) = max root (max (findMax l) (findMax r))
+
+tree1 = Node (Node (Node Empty 1 Empty) 3 (Node (Node Empty 4 Empty) 6 (Node Empty 7 Empty))) 8 (Node Empty 10 (Node (Node Empty 13 Empty) 14 Empty))
+tree2 = Node (Node (Node Empty 2 Empty) 3 (Node (Node Empty 4 Empty) 6 (Node Empty 7 Empty))) 8 (Node Empty 10 (Node (Node Empty 1 Empty) 14 Empty))
+tree3 = Node (Node (Node Empty 20 Empty) 3 (Node (Node Empty 4 Empty) 6 (Node Empty 7 Empty))) 8 (Node Empty 10 (Node (Node Empty 1 Empty) 14 Empty))
+tree4 = Node (Node Empty "e" Empty) "h" (Node Empty "l" Empty)
